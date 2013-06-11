1a3bf4a4976545046d23022c320d24349fa07d21
sylwiaw <sylwnek@gmail.com>
2013-06-11 15:45:01 +0300

    Update ComplexMeasurement.java

--- ComplexMeasurement.java
+++ ComplexMeasurement.java
@@ -2,64 +2,122 @@ package zespolowe;
 
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.Random;
 
 
-public class ComplexMeasurement implements Runnable{
-     ComplexMeasurement(int number,String operation){
-        this.number = number;
+/**
+ *
+ * @author Sylwia Wnek
+ */
+public class ComplexMeasurement implements Measurement{
+    ComplexMeasurement(String simpleName){
+        this.time = 5;
+        this.number = 30;
+        this.operation = "mean";
+        this.simpleName = simpleName;
+        this.type = "complex";
+    }
+    
+    /**
+     * 
+     * @param time przedzial czasu, dla ktorego wykonywane sa obliczenia
+     * @param operation nazwa wykonywanych obliczen
+     */
+     ComplexMeasurement(int time,String operation, String simpleName){
+        this.time = time;
+        this.number = time*6;
         this.operation = operation;
+        this.simpleName = simpleName;
     }
     
     int number;
+    int time;
     String operation;
-    LinkedList <Integer> data = new LinkedList();
+    String name; // potrzebne ?
+    String simpleName; //pomiar prosty, z ktorego pochodzi
+    String type;
+    boolean isComplex = true;
+    int id;
+    LinkedList <String> data = new LinkedList();
+    
     
     boolean dataCollected = false;
+    /**
+     * Wynik pomiaru.
+     */
     protected int result = -1;
+    /**
+     *
+     */
     protected String operationTime;
     int delay = 1000;
     
+    public String getSimpleName(){
+        return simpleName;
+    }
+    
+    public void setSimpleName(String name){
+        this.simpleName = name;
+    }
+   
+    /**
+     * Zwraca wynik pomiaru.
+     * @return
+     */
     public int getResult() {
         return result;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getOperationTime() {       
         return operationTime;
     }
     
+    public int getID(){
+        return id;
+    }
     
-    public void collectData(){
+    /**
+     * Zbiera dane.
+     */
+    public void collectData(String insertData){
         Random rand = new Random();
         if (data.size()<number){
-            data.push(rand.nextInt(100));
+            data.add(insertData);
         }
         else {
             dataCollected = true;
             data.poll();
-            Date date = new Date();
-            SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss:SS");
-            operationTime = f.format(date);
-            data.push(rand.nextInt(100));
+            data.add(insertData);
         }
     }
     
+    /**
+     * Liczy srednia.
+     */
     public void mean(){
         
         int i;
         int sum=0;
         
         for (i=0;i<number;i++){
-            sum += data.get(i);
+            sum += new Integer(data.get(i));
         }
         
         this.result = sum/number;
         
     }
 
-    public void chooseOperation(){
+    /**
+     * Wywoluje odpowiednia operacje na podstawie pola operation.
+     */
+    public void doOperation(){
         if (dataCollected){
             switch(operation){
                 case("mean") : this.mean();
@@ -70,49 +128,72 @@ public class ComplexMeasurement implements Runnable{
         }
     }
     
+    /**
+     * Znajduje maksymalna wartosc z zadanej ilosci pomiarow.
+     */
     public void maksimum(){
         
         int i;
         int max;
         
-        max = data.get(0);
+        max = new Integer(data.get(0));
         
         for (i=0;i<number;i++){
-            if (max<data.get(i)){max = data.get(i);}
+            if (max<new Integer(data.get(i))){max = new Integer(data.get(i));}
         }
         
         result = max;
     }
     
+    /**
+     * Znajduje minimalna wartosc z zadanej ilosci pomiarow.
+     */    
     public void minimum(){
         
         int i;
         int min;
         
-        min = data.get(0);
+        min = new Integer(data.get(0));
         
         for (i=0;i<number;i++){
-            if (min<data.get(i)){min = data.get(i);}
+            if (min<new Integer(data.get(i))){min = new Integer(data.get(i));}
         }
         
         this.result = min;
-    }    
+    }
     
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        ComplexMeasurement cm = new ComplexMeasurement(5,"mean");
-        cm.run();
+        ComplexMeasurement cm = new ComplexMeasurement(5,"mean","cpu");
+//        cm.run();
+    }
+
+
+    @Override
+    public void addData(long timestamp, String data) {
+        
     }
 
     @Override
-    public void run() {
-        while(true){
-            try{
-                this.collectData();
-                this.chooseOperation();
-                
-                Thread.sleep(delay);
-            }
-            catch(Exception e){}
-        }
-     }
+    public ArrayList<Data> getData() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public boolean isInitialized() {
+        return true;
+    }
+
+    @Override
+    public void setInitialized(boolean b) {
+        
+    }
 }
